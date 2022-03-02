62c5c751140763ba239bd284618915b0215604b8
Михаил Кондрашин <Kondrashin.mihail@outlook.com>
2022-03-02 09:53:05 +0300

    исправленная первая лабораторная работа

--- PrecastConcretePlantView/Program.cs
+++ PrecastConcretePlantView/Program.cs
@@ -55,22 +55,3 @@ namespace PrecastConcretePlantView
 
     }
 }
-
-
-/*namespace PrecastConcretePlantView
-{
-    internal static class Program
-    {
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new FormMain());
-        }
-    }
-}*/
